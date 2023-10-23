124cbfa2c9b468ebbe7ef893a6747395baaff965
PALABINDELA UDAYCHAND <udaychandpalabindela@gmail.com>
2023-10-23 10:57:04 +0530

    updates images

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ classnames = []
 with open('classes.txt','r') as f:
     classnames = f.read().splitlines()
 
-# print(classnames[0])
 
 image = cv2.imread(sys.argv[1])
 img = image.copy()
@@ -28,14 +27,13 @@ with torch.no_grad():
     print(ypred[0].keys())
 
     bbox,scores,labels = ypred[0]['boxes'],ypred[0]['scores'],ypred[0]['labels']
-    nums = torch.argwhere(scores > 0.80).shape[0]
+    nums = torch.argwhere(scores > 0.60).shape[0]
     for i in range(nums):
         x,y,w,h = bbox[i].numpy().astype('int')
         cv2.rectangle(img,(x,y),(w,h),(0,0,255),5)
         classname = labels[i].numpy().astype('int')
         classdetected = classnames[classname-1]
-        # print(classdetected)
-        cvzone.putTextRect(img,classdetected,[x,y+100],scale=2,border=2)
+        cvzone.putTextRect(img,classdetected,[x,y+100])
 
 cv2.imshow('Detection',img)
 cv2.waitKey(0)
